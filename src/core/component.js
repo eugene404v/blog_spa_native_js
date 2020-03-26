a3144b5d31f7ca913b776d5cebb78885870bb74d
Eugene Koryabkin <eugenewalknut@gmail.com>
2020-03-26 17:32:04 +0300

    component class, header class, show and hide methods

---
+++ src/core/component.js
@@ -0,0 +1,20 @@
+class Component {
+    constructor(id) {
+        this.$el = document.getElementById(id);
+        this.init();
+    }
+
+    init() {
+
+    }
+
+    hide() {
+        this.$el.classList.add('hide');
+    }
+
+    show() {
+        this.$el.classList.remove('hide');
+    }
+}
+
+export default Component;
