dc5199bb439d05c3235c7ca19d366d9b02a69f72
Eugene Koryabkin <eugenewalknut@gmail.com>
2020-04-30 21:54:25 +0300

    preloader for posts

--- src/components/posts.component.js
+++ src/components/posts.component.js
@@ -9,15 +9,17 @@ import {
 } from '../services/transform.service';
 
 export class PostsComponent extends Component {
-    constructor(id) {
+    constructor(id, {loader}) {
         super(id);
+        this.loader = loader;
     }
 
     async onShow() {
+        this.loader.show();
         const fbData = await apiService.fetchPosts();
         const posts = TransformService.fbObjectToArray(fbData);
         const html = posts.map(post => renderPost(post));
-
+        this.loader.hide();
         this.$el.insertAdjacentHTML('afterbegin', html.join(' '));
     }
 
