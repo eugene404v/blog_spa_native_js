38a1e79fc0a34169cc1ba96e823ad4918221707c
Eugene Koryabkin <eugenewalknut@gmail.com>
2020-05-01 21:21:48 +0300

    save favorites to localstorage

--- src/components/posts.component.js
+++ src/components/posts.component.js
@@ -14,6 +14,10 @@ export class PostsComponent extends Component {
         this.loader = loader;
     }
 
+    init() {
+        this.$el.addEventListener('click', buttonHandler.bind(this));
+    }
+
     async onShow() {
         this.loader.show();
         const fbData = await apiService.fetchPosts();
@@ -32,22 +36,51 @@ function renderPost(post) {
     const tag = post.type === 'news' ?
         '<li class="tag tag-blue tag-rounded">Новость</li>' :
         '<li class="tag tag-rounded">Заметка</li>';
-    const button = '<button class="button button-small button-round button-primary">SAVE</button>';
+    const button = (JSON.parse(localStorage.getItem('favorites')) || []).includes(post.id) ?
+    `<button class="button button-small button-round button-danger" data-id="${post.id}"> 
+            Удалить
+    </button>`:
+    `<button class="button button-small button-round button-primary" data-id="${post.id}"> 
+            Сохранить
+    </button>`;
     return `
-    <div class="panel">
-      <div class="panel-head">
-        <p class="panel-title">${post.title}</p>
-        <ul class="tags">
-            ${tag}
-        </ul>
-      </div>
-      <div class="panel-body">
-        <p class="multi-line">${post.fulltext}</p>
-      </div>
-      <div class="panel-footer w-panel-footer">
-        <small>${post.date}</small>
-        <span>${button}</span>
-      </div>
-    </div>
+        <div class="panel">
+            <div class="panel-head">
+                <p class="panel-title">${post.title}</p>
+                <ul class="tags">
+                    ${tag}
+                </ul>
+            </div>
+            <div class="panel-body">
+                <p class="multi-line">${post.fulltext}</p>
+            </div>
+            <div class="panel-footer w-panel-footer">
+                <small>${post.date}</small>
+                <span>${button}</span>
+            </div>
+        </div>
     `;
+}
+
+
+function buttonHandler(event) {
+    const $el = event.target;
+    const id = $el.dataset.id;
+    if (id) {
+        let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+
+        if (favorites.includes(id)) {
+            $el.textContent = "Сохранить";
+            $el.classList.add('button-primary');
+            $el.classList.remove('button-danger');
+            favorites = favorites.filter(fId => fId !== id);
+        } else {
+            $el.textContent = "Удалить";
+            $el.classList.remove('button-primary');
+            $el.classList.add('button-danger');
+            favorites.push(id);
+        }
+
+        localStorage.setItem('favorites', JSON.stringify(favorites));
+    }
 }
